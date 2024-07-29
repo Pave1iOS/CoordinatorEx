bb5e0821e492b754e3ba72bb3c196ca827ccefe0
Pavel <149584590+Pave1iOS@users.noreply.github.com>
2024-07-29 23:31:36 +0300

    Add files via upload

---
+++ CoordinatorEx/CoordinatorEx/CoordinatorExApp.swift
@@ -0,0 +1,19 @@
+//
+//  CoordinatorExApp.swift
+//  CoordinatorEx
+//
+//  Created by Pavel Gribachev on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorExApp: App {
+		
+    var body: some Scene {
+        WindowGroup {
+			CoordinatorView()
+        }
+		
+    }
+}
